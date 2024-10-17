6ea5ee1ff596347846c7c56abc71bb2e88b043b1
DavitJilavyan <David.jilavyan95@gmail.com>
2024-10-17 13:29:01 +0400

    homework

---
+++ HaykiDaser/Lesson2/operators/Lesson2/src/main/java/Operations.java
@@ -0,0 +1,9 @@
+public class Operations {
+    public static void main(String[] args) {
+        int a;
+        int A;
+        int $x;
+        int _x=9;
+        System.out.println(_x);
+    }
+}
